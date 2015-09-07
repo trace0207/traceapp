ca306061724043bad4aa7b197cc72324c268ce5d
cmcc <cmcc@tracedemacbook.local>
2015-09-07 14:17:34 +0800

    first commit

---
+++ tracedemo/tracedemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tracedemo
+//
+//  Created by cmcc on 15/9/7.
+//  Copyright (c) 2015年 trace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tracedemo/tracedemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tracedemo
+//
+//  Created by cmcc on 15/9/7.
+//  Copyright (c) 2015年 trace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
