5c13716a9e33d3ad07ae27da9b92076cc8fd3ec5
traceIcloud <luotianjia@icloud.com>
2016-03-12 15:47:38 +0800

    TEST

--- tracedemo/BClient/BHomePageViewController.m
+++ tracedemo/BClient/BHomePageViewController.m
@@ -22,6 +22,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.hidDefaultBackBtn = YES;
+    //SDFDSFDSFDS
     // Do any additional setup after loading the view from its nib.
     [self initView];
 }
